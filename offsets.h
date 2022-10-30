ee6d520a76856dfa3cf025fc15409a288ba86aa9
flat <76826123+FlatManDud@users.noreply.github.com>
2022-10-30 13:44:22 -0600

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Windows.h>
+namespace offsets
+{
+	//https://dumps.host/
+	DWORD uworld = 0x123;
+	DWORD gameinstance = 0x123;
+	DWORD localpawn = 0x123;
+	DWORD localplayer = 0x123;
+	DWORD playercontroller = 0x123;
+	DWORD persistentlevel = 0x123;
+	DWORD actorcount = 0x123;
+	DWORD aactors = 0x123;
+	DWORD componenttoworld = 0x123;
+	DWORD bonearray = 0x123;
+	DWORD currentactormesh = 0x123;
+}
